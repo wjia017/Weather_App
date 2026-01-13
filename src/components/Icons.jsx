ddedbc2b7675a56419e8735dfe5ba0e343cd3ebb
Lahana <lahana@Lahanas-MacBook-Pro.local>
2026-01-13 16:50:00 +0545

    Fix icons by using public assets for Vercel

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,16 +1,29 @@
-import React from 'react';
-import windIcon from '../assets/wind.png';
-import humidityIcon from '../assets/humidity.png';
-import visibilityIcon from '../assets/visibility.png';
-import sunriseIcon from '../assets/sunrise.png';
-import sunsetIcon from '../assets/sunset.png';
+import React from "react";
 
-const Icons =({src,alt,className}) => (
-    <img src={src} alt= {alt} className={`h-8 w-8 inline-block ${className}`}/>
+const Icons = ({ src, alt, className }) => (
+  <img
+    src={src}
+    alt={alt}
+    className={`h-8 w-8 inline-block ${className}`}
+  />
+);
 
-)
-export const WindIcon = () => <Icons src={windIcon} alt="Wind" className='animate-icon svg-hover'/>
-export const HumidityIcon = () => <Icons src={humidityIcon} alt="Humidity" className='powerful-pulse svg-hover'/>
-export const VisibilityIcon = () => <Icons src={visibilityIcon} alt="Visibility" className='powerful-pulse svg-hover'/>
-export const SunriseIcon = () => <Icons src={sunriseIcon} alt="Sunrise" className='powerful-pulse svg-hover'/>
-export const SunsetIcon = () => <Icons src={sunsetIcon} alt="Sunset" className='powerful-pulse svg-hover'/>
+export const WindIcon = () => (
+  <Icons src="/assets/wind.png" alt="Wind" className="powerful-pulse svg-hover" />
+);
+
+export const HumidityIcon = () => (
+  <Icons src="/assets/humidity.png" alt="Humidity" className="powerful-pulse svg-hover" />
+);
+
+export const VisibilityIcon = () => (
+  <Icons src="/assets/visibility.png" alt="Visibility" className="powerful-pulse svg-hover" />
+);
+
+export const SunriseIcon = () => (
+  <Icons src="/assets/sunrise.png" alt="Sunrise" className="powerful-pulse svg-hover" />
+);
+
+export const SunsetIcon = () => (
+  <Icons src="/assets/sunset.png" alt="Sunset" className="powerful-pulse svg-hover" />
+);
